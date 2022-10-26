8f250559540d60b315ade42f78dba4d39f48f7ea
Lily-Aung-Mylove <lilyKaung2003@gmail.com>
2022-10-26 14:16:57 +0630

    Quiz_Game

---
+++ question.js
@@ -0,0 +1,58 @@
+//creation an array and passing the number, questions, options and answers
+let questions = [
+    {
+        numb: 1,
+        question:"Which is the first Arakanese Kingdom in Rakhine?",
+        answer: "Dhanyawadi",
+        options: [  
+            "Dhanyawadi",
+            "Waithali",
+            "Lemro",
+            "Mrauk U"
+        ]
+    },
+    {
+        numb: 2,
+        question:"Where was  the location of a battle during the conquest of Arakan by the Burmese king Bodawpaya?",
+        answer: "Sittwe",
+        options: [  
+            "Mrauk Oo",
+            "Kyauk Taw",
+            "Sittwe",
+            "Min Bya"
+        ]
+    },
+    {
+        numb: 3,
+        question:"When was the Arakanese kingdom  conquered?",
+        answer: "December 31,1784",
+        options: [  
+            "December 20,1878",
+            "October 30,1784",
+            "December 31,1785",
+            "December 31,1784"
+        ]
+    },
+    {
+        numb: 4,
+        question:"When did Arakan become a state of the Socialist Republic of the Union of Burma?",
+        answer: "In 1973",
+        options: [  
+            "In 1875",
+            "In 1980",
+            "In 1975",
+            "In 1973"
+        ]
+    },
+    {
+        numb: 5,
+        question:"When did U Ottama die?",
+        answer: "9 September, 1939",
+        options: [  
+            "10 October,1874",
+            "9 November, 1798",
+            "9 September, 1939",
+            "31 December,1984"
+        ]
+    }
+];
